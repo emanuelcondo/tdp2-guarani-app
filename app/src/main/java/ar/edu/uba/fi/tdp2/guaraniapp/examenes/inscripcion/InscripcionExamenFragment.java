a56b3d6563a6bb3791fcc95807573e131ded8456
Hugo Chavar <hechavar@gmail.com>
2018-11-12 15:49:57 -0300

    Agrego mensaje cuando no hay examenes

--- app/src/main/java/ar/edu/uba/fi/tdp2/guaraniapp/examenes/inscripcion/InscripcionExamenFragment.java
+++ app/src/main/java/ar/edu/uba/fi/tdp2/guaraniapp/examenes/inscripcion/InscripcionExamenFragment.java
@@ -1,9 +1,14 @@
 package ar.edu.uba.fi.tdp2.guaraniapp.examenes.inscripcion;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import ar.edu.uba.fi.tdp2.guaraniapp.MainActivity;
 import ar.edu.uba.fi.tdp2.guaraniapp.R;
@@ -32,6 +37,10 @@ public class InscripcionExamenFragment extends RecyclerFragment {
             titulo = getString(R.string.seleccion_fechas_examenes);
         }
         ((MainActivity) getActivity()).setToolbarName(titulo);
+
+        if (getAdapter().getItemCount() == 0) {
+            addErrorMessage();
+        }
     }
 
     @Override
@@ -43,6 +52,15 @@ public class InscripcionExamenFragment extends RecyclerFragment {
     protected void configureAdapter() {
         InscripcionExamenAdapter adapter = new InscripcionExamenAdapter(getActivity(), this);
         this.setConfiguredAdapter(adapter);
+    }
+
+    private void addErrorMessage() {
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View error = inflater.inflate(R.layout.error_message, null);
+        TextView errorMessage = error.findViewById(R.id.error_message_text);
+        errorMessage.setText(R.string.error_no_inscripciones);
+        CoordinatorLayout view = getView().getRootView().findViewById(R.id.coordinator);
+        view.addView(error);
     }
 
 }
